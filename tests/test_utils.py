32c7b597fd709fcd75c066f3f9ec92d00158917b
venkatkrishna <venkatkrishna@moe>
2025-02-11 01:48:21 +0100

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,39 +11,39 @@ class TestFlatten(SetupTeardown):
 
 
 class TestUrlJoin(SetupTeardown):
-    def test_join_paths_with_leading_slash(self):
+    def test_joins_paths(self):
         path = url_join("/tmp", "test", "file")
         assert path == "/tmp/test/file"
 
-    def test_join_paths_with_trailing_slash(self):
+    def test_joins_paths_with_leading_slash(self):
+        path = url_join("/tmp", "/test", "file")
+        assert path == "/tmp/test/file"
+
+    def test_joins_paths_with_trailing_slash(self):
         path = url_join("/tmp/", "test/", "file")
         assert path == "/tmp/test/file"
 
-    def test_join_paths_with_empty_strings(self):
-        path = url_join("/tmp", "", "test", "file")
-        assert path == "/tmp/test/file"
-
-    def test_join_single_path_with_leading_slash(self):
+    def test_joins_single_path_with_leading_slash(self):
         path = url_join("/tmp")
         assert path == "/tmp"
 
-    def test_join_single_path_without_leading_slash(self):
+    def test_joins_single_path_without_leading_slash(self):
         path = url_join("tmp")
         assert path == "tmp"
 
-    def test_join_no_paths(self):
+    def test_joins_no_paths(self):
         path = url_join()
         assert path == ""
 
-    def test_join_full_uri(self):
+    def test_joins_full_uri(self):
         path = url_join("http://example.com", "path", "to", "resource")
         assert path == "http://example.com/path/to/resource"
 
-    def test_strip_bare_slashes(self):
+    def test_strips_bare_slashes(self):
         path = url_join("/", "/", "/")
         assert path == "/"
 
-    def test_join_paths_without_leading_slash(self):
+    def test_joins_paths_without_leading_slash(self):
         path = url_join("tmp", "test", "file")
         assert path == "tmp/test/file"
 
